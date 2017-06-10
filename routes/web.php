fad667f5675eeefd75442ba9e1c05d3f6ffc641b
KaelLim <kaelsohappy1@gmail.com>
2017-06-10 17:04:22 +0800

    報名系統

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,26 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('/', function () {
+    $data = ['name' => 'tad', 'say' => '嗨！'];
+    return view('welcome', $data);
+});
+
+Route::get('/action/create', function () {
+    return view('create');
+})
+    ->name('action.create');
+
+Route::post('/action', function () {
+    return view('store');
+})->name('action.store');
